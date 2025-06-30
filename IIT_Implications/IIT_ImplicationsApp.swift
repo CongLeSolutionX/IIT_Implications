9fa57b25460b68ab37ea958ce837ffe459edb3f5
Cong Le <longchike@gmail.com>
2025-06-30 02:12:02 -0700

    The setup

---
+++ IIT_Implications/IIT_ImplicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIT_ImplicationsApp.swift
+//  IIT_Implications
+//
+//  Created by Cong Le on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct IIT_ImplicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
